87a64be00c2dcad450c9832e5a7f98a698358850
Денис Даниленко <giqus123@gmail.com>
2020-08-28 15:55:44 +0300

    YLI

--- QuickMath/QuickMath/UserData/User.cs
+++ QuickMath/QuickMath/UserData/User.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,4 +9,4 @@ namespace QuickMath.UserData
     internal class User
     {
     }
-}
+} 
