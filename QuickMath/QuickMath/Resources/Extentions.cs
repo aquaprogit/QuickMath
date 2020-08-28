bcb5520969b33b5371270a679fdab26462889d4f
AquaVG <vladd.golovatyuk@gmail.com>
2020-08-28 12:32:04 +0300

    Initial commit

---
+++ QuickMath/QuickMath/Resources/Extentions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickMath.Resources
+{
+    public static class Extentions
+    {
+        public static int ToInt32<T>(this T self)
+        {
+            return Convert.ToInt32(self);
+        }
+        public static bool IsOneOf<T>(this T self, params T[] coll)
+        {
+            return coll.Contains(self);
+        }
+
+    }
+}
