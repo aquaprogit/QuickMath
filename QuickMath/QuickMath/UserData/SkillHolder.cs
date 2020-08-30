f4afcd6418bd2d2e7409e27b2d13d2e1dbe2e04e
AquaVG <vladd.golovatyuk@gmail.com>
2020-08-30 12:38:39 +0300

    Skill holder class

--- QuickMath/QuickMath/UserData/SkillHolder.cs
+++ QuickMath/QuickMath/UserData/SkillHolder.cs
@@ -1,17 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace QuickMath.UserData                             
+namespace QuickMath.UserData
 {
-    internal class SkillHolder
+    internal class SkillHolder : INotifyPropertyChanged
     {
-        private int level;
-        private int exp;
+        private const int targetExp = 100;
+        private int level = 1;
+        private int exp = 0;
 
-        public int Level { get; private set; }
-        public int Exp { get; set; }
+        public int Level
+        {
+            get => level;
+            private set {
+                level = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Exp
+        {
+            get => exp;
+            set {
+                exp = value;
+                if (exp >= targetExp)
+                {
+                    do
+                    {
+                        exp -= targetExp;
+                        Level++;
+                    } while (exp >= targetExp);
+                }
+                else if (exp < 0)
+                {
+                    do
+                    {
+                        exp += targetExp;
+                        Level--;
+                    } while (exp < 0);
+                }
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
-} 
+}
