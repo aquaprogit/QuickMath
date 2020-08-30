7f3dd0db1d714f6997d4bee3739096721396b8e0
AquaVG <vladd.golovatyuk@gmail.com>
2020-08-30 15:43:21 +0300

    Prepeared MemorySkillWorker

--- QuickMath/QuickMath/App.xaml.cs
+++ QuickMath/QuickMath/App.xaml.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
+using QuickMath.SkillWorker;
+
 namespace QuickMath
 {
     /// <summary>
--- QuickMath/QuickMath/SkillWorker/MemorySkillWorker.cs
+++ QuickMath/QuickMath/SkillWorker/MemorySkillWorker.cs
@@ -1,24 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
+using QuickMath.UserData;
 
 namespace QuickMath.SkillWorker
 {
     class MemorySkillWorker : SkillWorkerBase, ISkillWorker
     {
+        private int Level
+        {
+            get => MemorySkillHolder.Level;
+        }
+        private SkillHolder MemorySkillHolder;
         public void CheckAnswer(string question, string userAnswer)
         {
-            
+            //Сверить число из вопроса и ответ пользователя
+            //Если равны то Right++ если нет, Wrong++
         }
 
         public string GetQuestion()
         {
             string question = "";
-
-
+            /*
+             * Сгенерировать 4-значное число если Level от [1 до 3]
+             * 5-значное если от (3 - 7]
+             * 6- значное если от (7 до 10]
+             * 7-значное если больше 10
+            */
             return question;
+        }
+
+        public MemorySkillWorker(SkillHolder memSkillHolder)
+        {
+            MemorySkillHolder = memSkillHolder;
         }
     }
 }
